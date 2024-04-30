2ef942d3e046cc8c465a27f2f89753c3e8dd179d
Henrique Focchesatto Grion <henriquegrion@hotmail.com>
2024-04-30 10:00:43 -0300

    adicionado 2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,7 @@ class Home extends BaseController
     public function index()
     {
         $data = [
-            'titulo' => 'CE Controle de Estoque 1'
+            'titulo' => 'CE Controle de Estoque 2'
         ];
         echo view('/templates/header-html', $data);
         echo view('/templates/header');
